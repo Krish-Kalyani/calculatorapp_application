cb25ca23ae6e324bd5ad33f61d65e28816109464
Krish-Kalyani <kkalyani1@student.gsu.edu>
2024-10-03 14:26:26 -0400

    Third Commit To Add All Functions and Result

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,67 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _buttonPressed(String value) {
+    setState(() {
+      if (value == 'C') {
+        _clear();
+      } else if (value == '+' || value == '-' || value == '*' || value == '/') {
+        if (_operand1.isNotEmpty) {
+          _operator = value;
+        }
+      } else if (value == '=') {
+        if (_operand1.isNotEmpty &&
+            _operator.isNotEmpty &&
+            _operand2.isNotEmpty) {
+          _calculateResult();
+        }
+      } else {
+        if (_operator.isEmpty) {
+          _operand1 += value;
+          _displayText = _operand1;
+        } else {
+          _operand2 += value;
+          _displayText = _operand2;
+        }
+      }
+    });
+  }
+
+  void _calculateResult() {
+    double num1 = double.tryParse(_operand1) ?? 0;
+    double num2 = double.tryParse(_operand2) ?? 0;
+    double result = 0;
+
+    switch (_operator) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        if (num2 == 0) {
+          _displayText = 'Error';
+          return;
+        } else {
+          result = num1 / num2;
+        }
+        break;
+    }
+    if (result == result.toInt()) {
+      _displayText = result.toInt().toString();
+    } else {
+      _displayText = result.toString();
+    }
+
+    _operand1 = '';
+    _operand2 = '';
+    _operator = '';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
